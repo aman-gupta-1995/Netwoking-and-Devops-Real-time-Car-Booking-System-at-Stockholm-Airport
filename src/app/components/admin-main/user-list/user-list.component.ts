579c91fa75eec339d21cf60b5479185771b25ef8
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-10 12:39:33 +0530

    Update user-list.component.ts

--- src/app/components/admin-main/user-list/user-list.component.ts
+++ src/app/components/admin-main/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ActivatedRoute} from '@angular/router'
+import {ActivatedRoute} from '@angular/router' 
 import { Router } from '@angular/router'
 import {Job} from '../../models'
 import {User} from '../../models'
